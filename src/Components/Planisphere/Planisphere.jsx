f2b3c4fa721a0d65b96b1f1a972552b3a2a6512a
nicolasdelisee <nicolas.delisee3@gmail.com>
2022-11-24 20:13:42 +0100

    Argentina

--- src/Components/Planisphere/Planisphere.jsx
+++ src/Components/Planisphere/Planisphere.jsx
@@ -5,6 +5,7 @@ const Planisphere = () => {
     return (
         <div className='planisphere_img'>
             <img src="/src/assets/logos/map.png" alt="" />
+            <img className="argentina" src="src/assets/logos/valise01-black.png" alt="Argentine" />
         </div>
     );
 };
