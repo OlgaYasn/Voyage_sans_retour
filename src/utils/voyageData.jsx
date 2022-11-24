2aac250315564a7fa13c449f10a31eaaffe863e8
nicolasdelisee <nicolas.delisee3@gmail.com>
2022-11-24 16:15:22 +0100

    Data Nico

--- src/utils/voyageData.jsx
+++ src/utils/voyageData.jsx
@@ -1,302 +1,193 @@
 const voyageInfo = [
 {
-id_activity :,
-name_activity:, 
-description :,
-age_recommended:,
-name_guide:,
+id_activity: 1,
+name_activity: "Traverser la rue en Thailande", 
+description: "Le Code de la route existe en Thaïlande, même s'il est peu appliqué. C’est sûrement pour cela qu’il s’agit d’un des 3 pays au monde qui connaît le plus d’accidents de la route. les Thaïlandais sont très peu respectueux du Code de la route. Les véhicules sont nombreux et variés sur les chaussées :  Il en résulte une ambiance anarchique, qu'il vous faudra gérer avec sang froid. Les accidents sont fréquents et souvent mortels. Les limites de vitesse, à 50 km/h en ville et 100 km/h en campagne, ne sont pas respectées.",
+age_recommended: "18-70",
+name_guide: "Phoebe Buffay",
 image:,
-country:,
-city:,
+country: "Argentine",
+city: "Buneos Aires",
+price: 80,
+dangerousity: 7.8,
 },
 {
-id_activity : 	
-name_activity:
-description :
-age_recommended:	
-name_guide:
-image:
-country:
-city :
+id_activity: 6,	
+name_activity: "Ceuillir de la marijuana dans des exploitations clandestines",
+description: "Profitez de la nature et de l'ambiance paisible des exploitations agricoles en Bolivie. Partagez un moment de convivialité avec les autres travailleurs.",
+age_recommended: "18-70",
+name_guide: "Luna Lovegood",
+image:,
+country: "Bolivie",
+city: "La Paz",
+price: 20,
+dangerousity: 5.8,
 },
 {
-id_activity : 	
-name_activity:
-description :
-age_recommended:	
-name_guide:
-image:
-country:
-city :
+id_activity: 4, 	
+name_activity: "Chercher un woofing dans les favelas",
+description: "Favela, aussi écrit favéla, désigne les bidonvilles brésiliens, situés sur des terrains occupés illégalement et contrôlés le plus souvent par des trafiquants de drogue qui luttent entre eux pour obtenir les plus vastes territoires, très souvent malsains (marécages, pentes raides des collines), et dont les habitations sont construites avec des matériaux de récupération. Toutefois, il ne faut pas sous-estimer la hospitalité des habitants des ces quartiers. Partez à l'aventure chez des locaux et partagez un moment de convivialité avec eux.",
+age_recommended: "18-70",	
+name_guide: "Luna Lovegood",
+image: ,
+country: "Brésil",
+city: "Rio de Janeiro",
+price: 10,
+dangerousity: 8.6,
 },
 {
-id_activity : 	
-name_activity:
-description :
-age_recommended:	
-name_guide:
-image:
-country:
-city :
+id_activity: 16, 	
+name_activity: "Faire de la high-line entre deux montgolfières",
+description: "Vivez une expérience vertigineuse en marchant sur la high-line entre deux montgolfières. Tout ce qu'il faut c'est un peu d'équilibre et un testament à jour.",
+age_recommended: "70+",	
+name_guide: "Michael Scott",
+image: ,
+country: "Brésil",
+city: "São Paulo",
+price: 2250,
+dangerousity: 9.0,
 },
 {
-id_activity : 	
-name_activity:
-description :
-age_recommended:	
-name_guide:
-image:
-country:
-city :
+id_activity: 11, 	
+name_activity: "Monter la tour Burj Khalifa à pied",
+description: "Vous n'êtes pas sportif et vous êtes claustrophobe ? Ce n'est pas grave ! Vivez une expérience inédite d'arriver au sommet de la plus grande tour du Monde à pied.  ",
+age_recommended: "70+",	
+name_guide: "Michael Scott",
+image: ,
+country: "Émirats Arabe Unis",
+city: "Dubaï",
+price: 125,
+dangerousity: 7.0,
 },
 {
-id_activity : 	
-name_activity:
-description :
-age_recommended:	
-name_guide:
-image:
-country:
-city :
+id_activity: 15, 	
+name_activity: "Monter son premier site sans boucle infinie",
+description: "Vers l'infini et au delà a dit Buzz l'éclair en faisant tomber sa base de données externe. Ne faites pas la même erreur que lui et montez votre premier site sans tuto et sans formateur.",
+age_recommended: "18-70",	
+name_guide: "Michael Scott",
+image: ,
+country: "France",
+city: "Bordeaux",
+price: 7500,
+dangerousity: 10,
 },
 {
-id_activity : 	
-name_activity:
-description :
-age_recommended:	
-name_guide:
-image:
-country:
-city :
+id_activity: 2, 	
+name_activity: "Base Jump pour fêter ses 80 ans",
+description: "Vous êtes en quête d'une sensation de liberté ? Coupez tous les liens avec la réalité en sautant dans le vide absolu.",
+age_recommended: "70+",	
+name_guide: "Luna Lovegood",
+image: ,
+country: "Inde",
+city: "Himalaya",
+price: 2500,
+dangerousity: 9.1,
 },
 {
-id_activity : 	
-name_activity:
-description :
-age_recommended:	
-name_guide:
-image:
-country:
-city :
+id_activity: 3, 	
+name_activity: "Nouvel An au mont Everest",
+description: "L'ascension de l'Everest s'entreprend essentiellement au printemps, avant la mousson, mais ne vous laissez pas la météo de vous décourager. Passez un Nouvel An inoubliable sur les sommets de la montagne emblématique. Revivez l'aventure du film Everest et faites des photos sur les lieux du tournage du film.",
+age_recommended: "70+",	
+name_guide: "Luna Lovegood",
+image: ,
+country: "Népal",
+city: "Katmandou",
+price: 3800,
+dangerousity: 9.3,
 },
 {
-id_activity : 	
-name_activity:
-description :
-age_recommended:	
-name_guide:
-image:
-country:
-city :
+id_activity: 5, 	
+name_activity: "Participer à la Gay Pride en Russie",
+description: "Malgré les apparences le peuple russe peut montrer une grande ouverture d'esprit. Partagez une ambiance chaleureuse d'un LGBT pride sur l'emblématique place Rouge aux portes de Kremlin.",
+age_recommended: "18-70",	
+name_guide: "Luna Lovegood",
+image: ,
+country: "Russie",
+city: "Moscou",
+price: 250,
+dangerousity: 7.9,
 },
 {
-id_activity : 	
-name_activity:
-description :
-age_recommended:	
-name_guide:
-image:
-country:
-city :
+id_activity: 7, 	
+name_activity: "Se désalterer dans le Gange",
+description: "Des millions de fidèles trouvent le salut en se baignant dans ses flots et en buvant son eau. Ce fleuve sacré contient les pires poisons : métaux lourds, matières fécales et produits chimiques intoxiquent lentement mais sûrement ses pèlerins.",
+age_recommended: "18-70",	
+name_guide: "Luna Lovegood",
+image: ,
+country: "Inde",
+city: "New Delhi",
+price: 15,
+dangerousity: 7.1,
 },
 {
-id_activity : 	
-name_activity:
-description :
-age_recommended:	
-name_guide:
-image:
-country:
-city :
+id_activity: 8, 	
+name_activity: "Marcher en déambulateur sur la piste cyclable à Amsterdam",
+description: "Il y a 400 km de pistes cyclables dans Amsterdam , on a le droit de prendre les rues à contre-sens, traverser les parcs à vélo et les feux sont réglés de manière à donner la priorité au vélos. D'ailleurs la règle est simple : les vélos ont toujours priorité – ici les cyclistes règnent en maître sur la ville. Tentez le défi de vous balader sur la piste cyclable tout en prenant votre temps.",
+age_recommended: "70+",	
+name_guide: "Luna Lovegood",
+image: ,
+country: "Pays-Bas",
+city: "Amsterdam",
+price: 50,
+dangerousity: 7.3,
 },
 {
-id_activity : 	
-name_activity:
-description :
-age_recommended:	
-name_guide:
-image:
-country:
-city :
+id_activity: 9, 	
+name_activity: "Courses de buffles d'eau",
+description: "Tous les ans à Chonburi, dans l’Est de la Thaïlande, se célèbre une compétition pour rendre hommage aux buffles et exprimer une grande gratitude à ces animaux qui travaillent pour les agriculteurs tout au long de l’année. Assistez à ces courses rapides, impressionnantes et franchement dangereuses!",
+age_recommended: "70+",	
+name_guide: "Luna Lovegood",
+image: ,
+country: "Thaïlande",
+city: "Chonburi",
+price: 300,
+dangerousity: 7.5,
 },
 {
-id_activity : 	
-name_activity:
-description :
-age_recommended:	
-name_guide:
-image:
-country:
-city :
+id_activity: 10, 	
+name_activity: "Marathon des Sables",
+description: "Se déroulant à chaque année au mois d’avril, ce marathon prenant place dans le Sud du Maroc est une course d’environ 240 kilomètres pendant laquelle les participants doivent porter sur leur dos la nourriture suffisante pour une semaine. Une épreuve sportive extrêmement difficile qui se déroule dans la chaleur du désert, sous des températures extrêmes pouvant avoisiner les 50 degrés.",
+age_recommended: "70+",	
+name_guide: "Michael Scott",
+image: ,
+country: "Maroc",
+city: "Casablanca",
+price: 180,
+dangerousity: 7.3,
 },
 {
-id_activity : 	
-name_activity:
-description :
-age_recommended:	
-name_guide:
-image:
-country:
-city :
+id_activity: 12, 	
+name_activity: "Monter El Capitan sans corde",
+description: "Aujourd'hui c'est El Capitan, roi de la vallée qui apparaît en fond d'écran par défaut lorsqu'ils ouvrent leur Mac. La falaise de 1 200 m dépourvue de fissures est l'un des plus fameux spots de grimpe au monde avec des routes qui durent de 2 à 7 jours. Allez au-delà de vos peurs et monter la falaise sans protection.",
+age_recommended: "18-70",	
+name_guide: "Michael Scott",
+image: ,
+country: "USA",
+city: "Parc National Yosemite",
+price: 250,
+dangerousity: 8.5,
 },
 {
-id_activity : 	
-name_activity:
-description :
-age_recommended:	
-name_guide:
-image:
-country:
-city :
+id_activity: 13, 	
+name_activity: "Nourrir une murène aux antilles",
+description: "Vous êtes passionnés (ou pas) par le monde sous-marin ? Vivez un moment de complicité avec les murènes en les donnant à manger.",
+age_recommended: "18-70",	
+name_guide: "Michael Scott",
+image: ,
+country: "Tahiti",
+city: "Rangiroa",
+price: 150,
+dangerousity: 8.7,
 },
 {
-id_activity : 	
-name_activity:
-description :
-age_recommended:	
-name_guide:
-image:
-country:
-city :
+id_activity: 14, 	
+name_activity: "Faire un safari à pied",
+description: "Vous avez marre d'être enfermé dans une voiture en voyant les animaux quelque part au loin ...? Nous aussi ! C'est pour cela qu'on vous propose le premier safari au monde à pied. Vous verrez les animaux de près et peut-être que vous pourrez même les carresser.",
+age_recommended: "70+",	
+name_guide: "Michael Scott",
+image: ,
+country: "Namibi",
+city: "Windhoek",
+price: 300,
+dangerousity: 8.7,
 },
-{
-id_activity : 	
-name_activity:
-description :
-age_recommended:	
-name_guide:
-image:
-country:
-city :
-},
-{
-id_activity : 	
-name_activity:
-description :
-age_recommended:	
-name_guide:
-image:
-country:
-city :
-},
-{
-id_activity : 	
-name_activity:
-description :
-age_recommended:	
-name_guide:
-image:
-country:
-city :
-},
-{
-id_activity : 	
-name_activity:
-description :
-age_recommended:	
-name_guide:
-image:
-country:
-city :
-},
-{
-id_activity : 	
-name_activity:
-description :
-age_recommended:	
-name_guide:
-image:
-country:
-city :
-},
-{
-id_activity : 	
-name_activity:
-description :
-age_recommended:	
-name_guide:
-image:
-country:
-city :
-},
-{
-id_activity : 	
-name_activity:
-description :
-age_recommended:	
-name_guide:
-image:
-country:
-city :
-},
-{
-id_activity : 	
-name_activity:
-description :
-age_recommended:	
-name_guide:
-image:
-country:
-city :
-},
-{
-id_activity : 	
-name_activity:
-description :
-age_recommended:	
-name_guide:
-image:
-country:
-city :
-},
-{
-id_activity : 	
-name_activity:
-description :
-age_recommended:	
-name_guide:
-image:
-country:
-city :
-},
-{
-id_activity : 	
-name_activity:
-description :
-age_recommended:	
-name_guide:
-image:
-country:
-city :
-},
-{
-id_activity : 	
-name_activity:
-description :
-age_recommended:	
-name_guide:
-image:
-country:
-city :
-},
-{
-id_activity : 	
-name_activity:
-description :
-age_recommended:	
-name_guide:
-image:
-country:
-city :
-},
-{
-id_activity : 	
-name_activity:
-description :
-age_recommended:	
-name_guide:
-image:
-country:
-city :
-}
-]
