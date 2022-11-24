f9109b84506e1a4cecf77fbbdba800a85ee3d96b
Olga Yasnopolskaya <olga_yasn@hotmail.com>
2022-11-24 14:01:22 +0100

    added voyage info

---
+++ src/utils/voyageData.jsx
@@ -0,0 +1,302 @@
+const voyageInfo = [
+{
+id_activity :,
+name_activity:, 
+description :,
+age_recommended:,
+name_guide:,
+image:,
+country:,
+city:,
+},
+{
+id_activity : 	
+name_activity:
+description :
+age_recommended:	
+name_guide:
+image:
+country:
+city :
+},
+{
+id_activity : 	
+name_activity:
+description :
+age_recommended:	
+name_guide:
+image:
+country:
+city :
+},
+{
+id_activity : 	
+name_activity:
+description :
+age_recommended:	
+name_guide:
+image:
+country:
+city :
+},
+{
+id_activity : 	
+name_activity:
+description :
+age_recommended:	
+name_guide:
+image:
+country:
+city :
+},
+{
+id_activity : 	
+name_activity:
+description :
+age_recommended:	
+name_guide:
+image:
+country:
+city :
+},
+{
+id_activity : 	
+name_activity:
+description :
+age_recommended:	
+name_guide:
+image:
+country:
+city :
+},
+{
+id_activity : 	
+name_activity:
+description :
+age_recommended:	
+name_guide:
+image:
+country:
+city :
+},
+{
+id_activity : 	
+name_activity:
+description :
+age_recommended:	
+name_guide:
+image:
+country:
+city :
+},
+{
+id_activity : 	
+name_activity:
+description :
+age_recommended:	
+name_guide:
+image:
+country:
+city :
+},
+{
+id_activity : 	
+name_activity:
+description :
+age_recommended:	
+name_guide:
+image:
+country:
+city :
+},
+{
+id_activity : 	
+name_activity:
+description :
+age_recommended:	
+name_guide:
+image:
+country:
+city :
+},
+{
+id_activity : 	
+name_activity:
+description :
+age_recommended:	
+name_guide:
+image:
+country:
+city :
+},
+{
+id_activity : 	
+name_activity:
+description :
+age_recommended:	
+name_guide:
+image:
+country:
+city :
+},
+{
+id_activity : 	
+name_activity:
+description :
+age_recommended:	
+name_guide:
+image:
+country:
+city :
+},
+{
+id_activity : 	
+name_activity:
+description :
+age_recommended:	
+name_guide:
+image:
+country:
+city :
+},
+{
+id_activity : 	
+name_activity:
+description :
+age_recommended:	
+name_guide:
+image:
+country:
+city :
+},
+{
+id_activity : 	
+name_activity:
+description :
+age_recommended:	
+name_guide:
+image:
+country:
+city :
+},
+{
+id_activity : 	
+name_activity:
+description :
+age_recommended:	
+name_guide:
+image:
+country:
+city :
+},
+{
+id_activity : 	
+name_activity:
+description :
+age_recommended:	
+name_guide:
+image:
+country:
+city :
+},
+{
+id_activity : 	
+name_activity:
+description :
+age_recommended:	
+name_guide:
+image:
+country:
+city :
+},
+{
+id_activity : 	
+name_activity:
+description :
+age_recommended:	
+name_guide:
+image:
+country:
+city :
+},
+{
+id_activity : 	
+name_activity:
+description :
+age_recommended:	
+name_guide:
+image:
+country:
+city :
+},
+{
+id_activity : 	
+name_activity:
+description :
+age_recommended:	
+name_guide:
+image:
+country:
+city :
+},
+{
+id_activity : 	
+name_activity:
+description :
+age_recommended:	
+name_guide:
+image:
+country:
+city :
+},
+{
+id_activity : 	
+name_activity:
+description :
+age_recommended:	
+name_guide:
+image:
+country:
+city :
+},
+{
+id_activity : 	
+name_activity:
+description :
+age_recommended:	
+name_guide:
+image:
+country:
+city :
+},
+{
+id_activity : 	
+name_activity:
+description :
+age_recommended:	
+name_guide:
+image:
+country:
+city :
+},
+{
+id_activity : 	
+name_activity:
+description :
+age_recommended:	
+name_guide:
+image:
+country:
+city :
+},
+{
+id_activity : 	
+name_activity:
+description :
+age_recommended:	
+name_guide:
+image:
+country:
+city :
+}
+]
