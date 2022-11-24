ebfb9b022ac634d779fecd9838e23afd0eef57f3
nicolasdelisee <nicolas.delisee3@gmail.com>
2022-11-24 21:46:22 +0100

    voyageDetails done

--- src/Components/Planisphere/Planisphere.jsx
+++ src/Components/Planisphere/Planisphere.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import "./Planisphere.css"
+import "./Planisphere.css";
+import Countries from "../../utils/voyageDetails";
+
 
 const Planisphere = () => {
+    console.log(Countries.name)
     return (
         <div className='planisphere_img'>
             <img src="/src/assets/logos/map.png" alt="" />
-            <img className="argentina" src="src/assets/logos/valise01-black.png" alt="Argentine" />
+            <div>{Countries.map((country) => (
+                <img key={country.id} className={country.className} src="src/assets/logos/valise01-black.png" alt={country.name} />))}
+            </div>
         </div>
     );
 };
