e39a42a724c6d1f737986594cad481096bddb473
Maxime Dupin <maximedupin@live.fr>
2022-11-24 15:01:53 +0100

    Structure WIP

--- src/Pages/HomePage/Home.jsx
+++ src/Pages/HomePage/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import Destination from '../../Components/Destination/Destination';
+import Navbar from '../../Components/Navbar/Navbar';
+import Planisphere from '../../Components/Planisphere/Planisphere';
 import "./Home.css";
 
 const Home = () => {
     return (
         <div>
-            
+            <h1>Home</h1>
+            <Destination />
+            <Navbar />
+            <Planisphere />
         </div>
     );
 };
