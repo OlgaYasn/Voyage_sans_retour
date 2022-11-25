5e2e605f237ecd9c05fdfbaf9de4bb733de8c194
nicolasdelisee <nicolas.delisee3@gmail.com>
2022-11-25 09:50:41 +0100

    Map en dur

--- src/Components/Planisphere/Planisphere.jsx
+++ src/Components/Planisphere/Planisphere.jsx
@@ -1,18 +1,15 @@
 import React from 'react';
 import "./Planisphere.css";
-import { voyageDetails } from '../../utils/voyageDetails';
-
+// import {map} from "src/assets/images/map.png";
 
 const Planisphere = () => {
-    const handleClickMap = (e) => {
-        console.log(e.target)
-    }
     return (
         <div className='planisphere'>
-            <img className="planisphere_img" src="/src/assets/logos/map.png" alt="" />
-            <div>{voyageDetails.map((country) => (
+            <img className="planisphere_img" src="src/assets/images/map.png" alt="carte du Monde" />
+            {/* <img className="planisphere_img" src="/src/assets/logos/map.png" alt="" />
+            <div className="valises">{voyageDetails.map((country) => (
                 <img onClick={handleClickMap} key={country.id} className={country.className_planisphere} src="src/assets/logos/valise-white.png" alt={country.name} />))}
-            </div>
+            </div> */}
         </div>
     );
 };
