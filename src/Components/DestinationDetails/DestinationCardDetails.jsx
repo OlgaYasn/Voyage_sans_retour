c5314fe3173a410a579d71f761976209a05cd81e
Cedric_Mounier <cdricmounier@gmail.com>
2022-11-25 01:51:17 +0100

    passage de props card details

---
+++ src/Components/DestinationDetails/DestinationCardDetails.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import "./DestinationCardDetails.css";
+
+
+const DestinationCardDetails = ({ activity, description, age, guide, image, country, city, price, dangerosity }) => {
+    return (
+        <div className="cardDetail">
+            <img className="PictureDetails" src={image} />
+            <h1 className="NameActivity">{activity}</h1>
+            <img className="Buy" src="./src/assets/logos/valise01-black.png" />
+            <p className="DescriptionActivity">{description}</p>
+            <p className="Guide">{guide}</p>
+            <div className="FuckingDangerosity">
+                <img className="pictoDead" src="./src/assets/logos/picto-cardDetails.png" />
+                <p>Taux de dangerosité : {dangerosity}</p>
+            </div>
+            <p className="Age">Age recommandé : {age}</p>
+            <p className="Price">{price} €</p>
+            <p className="Country">{country} {city}</p>
+
+        </div>
+    )
+}
+
+export default DestinationCardDetails;
