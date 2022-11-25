43e5e09154dbd7d7c59dc0bc25aab10c716077e7
Olga Yasnopolskaya <olga_yasn@hotmail.com>
2022-11-25 10:22:55 +0100

    merged anthony

--- src/Components/miniCard/MiniCard.jsx
+++ src/Components/miniCard/MiniCard.jsx
@@ -1,10 +1,5 @@
 /* eslint-disable react/prop-types */
-<<<<<<< HEAD
-import React from 'react';
-import "./MiniCard.css"
-=======
 import React from 'react'
->>>>>>> 726248fe25354517f510d4454e72f807ae257f05
 
 const MiniCard = ({ country, activity }) => {
   return (
@@ -15,24 +10,4 @@ const MiniCard = ({ country, activity }) => {
   )
 }
 
-<<<<<<< HEAD
-const MiniCard = ({country, activity}) => {
-    return (
-      <div className="miniCard_display">
-        <div className="group_text">
-          <p className="miniCard_title">{country}</p>
-          <p className="miniCard_text">{activity}</p>
-        </div>
-        <img
-          src="../src/assets/logos/pictoDead.png"
-          alt="image"
-          className="card_logo"
-        />
-      </div>
-    );
-};
-
-export default MiniCard;
-=======
 export default MiniCard
->>>>>>> 726248fe25354517f510d4454e72f807ae257f05
