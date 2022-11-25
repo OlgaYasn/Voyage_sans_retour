83f0853192578201dd2532fb7c2cd9b8f3f5c27c
ANTHONY LABADIE <anthony.labadie@outlook.fr>
2022-11-25 02:24:55 +0100

    Update

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,36 +1,29 @@
-import React from 'react';
-<<<<<<< HEAD
-import { Link } from 'react-router-dom';
-=======
-import { NavLink } from "react-router-dom";
->>>>>>> b8487fdca128d2d1f80db9c1bf66e401272e8b21
-import "./Navbar.css";
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './Navbar.css'
 
 const Navbar = () => {
-    return (
-<<<<<<< HEAD
-  <div className='navbar'>
-
+  return (
+    <div className="navbar">
       <Link to="/">
         <img
           className="navbar__img"
           src="src/assets/logos/logo-inline-white.png"
           alt="image intro"
         />
+      </Link>
+      <img
+        className="navbar__img__valise"
+        src="src/assets/logos/valise-white.png"
+        alt="image intro"
+      />
+      <div className="navBar">
+        <Link to="/reservations">
+          <h1>Réservations</h1>
         </Link>
-        <img
-          className="navbar__img__valise"
-          src="src/assets/logos/valise-white.png"
-          alt="image intro"
-        />
+      </div>
+    </div>
+  )
+}
 
-  </div>
-=======
-        <div className='navBar'>
-            <NavLink to="/reservations"><h1>Réservations</h1></NavLink> 
-        </div>
->>>>>>> b8487fdca128d2d1f80db9c1bf66e401272e8b21
-    );
-};
-
-export default Navbar;
+export default Navbar
