037f0eaa88d5ce2fc80169c107715db4c7415cb5
ANTHONY LABADIE <anthony.labadie@outlook.fr>
2022-11-25 11:22:04 +0100

    Update branch

--- src/Components/DestinationDetails/DestinationCardDetails.jsx
+++ src/Components/DestinationDetails/DestinationCardDetails.jsx
@@ -1,40 +1,42 @@
-import React, { useState } from 'react';
-import PropTypes from "prop-types";
-import "./DestinationCardDetails.css";
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+import './DestinationCardDetails.css'
 
+const DestinationCardDetails = ({
+  activity,
+  description,
+  age,
+  guide,
+  image,
+  country,
+  city,
+  price,
+  dangerosity,
+}) => {
+  const [cancelButton, setCancelButton] = useState(true)
 
-const DestinationCardDetails = ({ activity, description, age, guide, image, country, city, price, dangerosity }) => {
-    
-    const [cancelButton, setCancelButton] = useState(true);
-    
-    const handleClick = () => {
-        setCancelButton(!cancelButton);
-    }
-    
-    
-    return (
-        <div className="cardDetail">
-                <div>
-                     <img className = "cancelButton" onClick={handleClick} src="src/assets/logos/cancel.png" alt="logo" />
-            <img className="PictureDetails" src={image} />
-            <h1 className="NameActivity">{activity}</h1>
-            <img className="Buy" src="./src/assets/logos/valise-white.png" />
-            <p className="DescriptionActivity">{description}</p>
-            <p className="Guide">Votre guide : {guide}</p>
-            <div className="FuckingDangerosity">
-                <img className="pictoDead" src="./src/assets/logos/pictoDead.png" />
-                <p>Taux de dangerosité : {dangerosity}</p>
-            </div>
-            <p className="Age">Age recommandé : {age} ans</p>
-            <p className="Price">{price} €</p>
-            <p className="Country">
-                {country} / {city}
-            </p>
-                </div>
-           
-        </div>
-        
-    );
+  const handleClick = () => {
+    setCancelButton(!cancelButton)
+  }
+
+  return (
+    <div className="cardDetail">
+      <img className="PictureDetails" src={image} />
+      <h1 className="NameActivity">{activity}</h1>
+      <img className="Buy" src="./src/assets/logos/valise-white.png" />
+      <p className="DescriptionActivity">{description}</p>
+      <p className="Guide">Votre guide : {guide}</p>
+      <div className="FuckingDangerosity">
+        <img className="pictoDead" src="./src/assets/logos/pictoDead.png" />
+        <p>Taux de dangerosité : {dangerosity}</p>
+      </div>
+      <p className="Age">Age recommandé : {age} ans</p>
+      <p className="Price">{price} €</p>
+      <p className="Country">
+        {country} / {city}
+      </p>
+    </div>
+  )
 }
 DestinationCardDetails.propTypes = {
   activity: PropTypes.func.isRequired,
@@ -46,5 +48,5 @@ DestinationCardDetails.propTypes = {
   city: PropTypes.func.isRequired,
   price: PropTypes.arrayOf.isRequired,
   dangerosity: PropTypes.arrayOf.isRequired,
-};
+}
 export default DestinationCardDetails
