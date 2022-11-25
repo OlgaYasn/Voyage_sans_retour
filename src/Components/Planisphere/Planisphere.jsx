b59665e4dcc068b46fb9587410797377ddb6db7f
nicolasdelisee <nicolas.delisee3@gmail.com>
2022-11-25 03:47:08 +0100

    Pays placés ntm

--- src/Components/Planisphere/Planisphere.jsx
+++ src/Components/Planisphere/Planisphere.jsx
@@ -4,11 +4,14 @@ import { voyageDetails } from '../../utils/voyageDetails';
 
 
 const Planisphere = () => {
+    const handleClickMap = (e) => {
+        console.log(e.target)
+    }
     return (
         <div className='planisphere'>
             <img className="planisphere_img" src="/src/assets/logos/map.png" alt="" />
             <div>{voyageDetails.map((country) => (
-                <img key={country.id} className={country.className_planisphere} src="src/assets/logos/valise-white.png" alt={country.name} />))}
+                <img onClick={handleClickMap} key={country.id} className={country.className_planisphere} src="src/assets/logos/valise-white.png" alt={country.name} />))}
             </div>
         </div>
     );
